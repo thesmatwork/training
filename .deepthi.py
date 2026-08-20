8ddbb9372238c4b5a139d28486f1bcd80c90af04
kdeepthi11 <kdeepthi@thesmatwork.com>
2026-08-20 13:02:48 +0530

    Update .deepthi.py

--- .deepthi.py
+++ .deepthi.py
@@ -1,5 +1,9 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
 
-print("Hello", name)
-print("You are", age, "years old.")
+import random
+used_number=set()
+for user in range(5):
+    number=random.randint(100000,999999)
+    while number in used_number:
+        number=random.randint(10000,17680)
+    used_number.add(number)
+    print("user",user+1,":",number)
