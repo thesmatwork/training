15048de39d5fd348f1a4c71482c18d4387eaeba7
saiprasanna2517 <esprasanna@thesmatwork.com>
2026-08-20 13:04:59 +0530

    Update saiprasanna.py

--- saiprasanna.py
+++ saiprasanna.py
@@ -1,14 +1,5 @@
-number = int(input("Enter a number: "))
-if number < 2:
-    print("Not a prime number")
-else:
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-    if is_prime:
-        print("Prime number")
-    else:
-        print("Not a prime number")    
+name=input("Enter your name: ")
+print(len(name))
+print(name.upper())
+print(name.lower())
             
