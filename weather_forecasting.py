72bfb969c30cc738c4ca65882cb16026bacf226d
saiprasanna2517 <esprasanna@thesmatwork.com>
2026-08-20 16:23:57 +0530

    Update weather_forecasting.py

--- weather_forecasting.py
+++ weather_forecasting.py
@@ -1,4 +1,13 @@
 import requests
-response=requests.get("https://api.github.com")
-print(response.status_code)
-print(response.json())
+city=input("enter city name: ")
+api_key="c74c1ad229b52a9831ae12898c1f4797"
+url = "https://api.openweathermap.org/data/2.5/weather"
+params = {
+    "q": city,
+    "appid": api_key,
+    "units": "metric"
+}
+response = requests.get(url, params=params)
+data = response.json()
+temperature = data["main"]["temp"]
+print("Temperature:", temperature, "°C")
