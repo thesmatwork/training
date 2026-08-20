07d14e331e684d405a4728553e9b5900e570523e
saiprasanna2517 <esprasanna@thesmatwork.com>
2026-08-20 12:41:39 +0530

    Update saiprasanna.py

--- saiprasanna.py
+++ saiprasanna.py
@@ -7,3 +7,8 @@ print(d)
 i=int(input("Enter the first number: "))
 j=int(input("Enter the second number: "))
 print("The result is: ",a*b)
+number=int(input("Enter a number: "))
+if number % 2==0:
+    print("The number is even")
+else:
+    print("The number is odd")   
