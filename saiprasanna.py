62ddd6fc2e3cf6aff1cce2f7795a2d97638634a7
saiprasanna2517 <esprasanna@thesmatwork.com>
2026-08-20 12:36:55 +0530

    Update saiprasanna.py

--- saiprasanna.py
+++ saiprasanna.py
@@ -1,2 +1,9 @@
-print("Hello")
-print("Welcome")
+a=10
+b=20
+c=a+b
+print(c)
+d=a-c
+print(d)
+i=int(input("Enter the first number: "))
+j=int(input("Enter the second number: "))
+print("The result is: ",a*b)
