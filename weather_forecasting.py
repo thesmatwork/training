fa1d668b69cc036a5b819d883afdc8595a6bf3b6
saiprasanna2517 <esprasanna@thesmatwork.com>
2026-08-21 11:04:34 +0530

    Update weather_forecasting.py

--- weather_forecasting.py
+++ weather_forecasting.py
@@ -1,13 +1,19 @@
 import requests
-city=input("enter city name: ")
-api_key="c74c1ad229b52a9831ae12898c1f4797"
-url = "https://api.openweathermap.org/data/2.5/weather"
-params = {
-    "q": city,
-    "appid": api_key,
-    "units": "metric"
-}
-response = requests.get(url, params=params)
-data = response.json()
-temperature = data["main"]["temp"]
-print("Temperature:", temperature, "°C")
+from fastapi import FastAPI
+app = FastAPI()
+api_key = "c74c1ad229b52a9831ae12898c1f4797"
+@app.get("/weather")
+def get_weather(city: str):
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    temperature = data["main"]["temp"]
+    return {
+        "city": city,
+        "temperature": temperature
+    }
