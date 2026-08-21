68c20e188dc0a6da36d3ba23c108619e4d7a7c58
saiprasanna2517 <esprasanna@thesmatwork.com>
2026-08-21 11:58:48 +0530

    Update weather_forecasting.py

--- weather_forecasting.py
+++ weather_forecasting.py
@@ -1,19 +1,26 @@
 import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 api_key = "c74c1ad229b52a9831ae12898c1f4797"
-@app.get("/weather")
-def get_weather(city: str):
+class WeatherRequest(BaseModel):
+    city: str
+@app.post("/weather")
+def get_weather(request: WeatherRequest):
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
-        "q": city,
+        "q": request.city,
         "appid": api_key,
         "units": "metric"
     }
+
     response = requests.get(url, params=params)
+
     data = response.json()
+
     temperature = data["main"]["temp"]
+
     return {
-        "city": city,
+        "city": request.city,
         "temperature": temperature
     }
