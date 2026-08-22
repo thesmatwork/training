a99ceea59c671e3f9948dce581fe8b9b65d3ccc7
saiprasanna2517 <esprasanna@thesmatwork.com>
2026-08-22 11:29:29 +0530

    Update weather_forecasting.py

--- forecast project/weather_forecasting.py
+++ forecast project/weather_forecasting.py
@@ -13,30 +13,21 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 api_key = "c74c1ad229b52a9831ae12898c1f4797"
-
-
 class WeatherRequest(BaseModel):
     city: str
-
-
 @app.post("/weather")
 def get_weather(request: WeatherRequest):
-
     url = "https://api.openweathermap.org/data/2.5/weather"
-
     params = {
         "q": request.city,
         "appid": api_key,
         "units": "metric"
     }
-
     response = requests.get(url, params=params)
     data = response.json()
-
+    print(data)
     temperature = data["main"]["temp"]
-
     return {
         "city": request.city,
         "temperature": temperature
